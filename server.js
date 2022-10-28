951041c486376c58ee01bcaefe21eb9fa3cdceab
NinaSerebryakova <serebr.nina@gmail.com>
2022-10-28 11:42:26 -0400

    created a single post view

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 let PORT = process.env.PORT || 3001;
 
-const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const hbs = exphbs.create({});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
